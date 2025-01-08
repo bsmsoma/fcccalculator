15bf9bb4e51545a785d7062c30bcfd56aed25118
bsmsoma <soma.bsm@gmail.com>
2025-01-08 18:27:47 -0300

    another chalenge for FCC

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container ">
+        <div className="calculator">
+          <div className="display" id="display">0</div>
+          <div className="buttons">
+            <button className="btn clear" id="clear">AC</button>
+            <button className="btn operator" id="divide">/</button>
+            <button className="btn operator" id="multiply">*</button>
+            <button className="btn operator" id="minus">-</button>
+            <button className="btn" id="serven">7</button>
+            <button className="btn" id="eight">8</button>
+            <button className="btn" id="nine">9</button>
+            <button className="btn operator" id="plus">+</button>
+            <button className="btn" id="five">4</button>
+            <button className="btn" id="five">5</button>
+            <button className="btn" id="six">6</button>
+            <button className="btn" id="decimal">.</button>
+            <button className="btn" id="one">1</button>
+            <button className="btn" id="two">2</button>
+            <button className="btn" id="three">3</button>
+            <button className="btn equals" id="equals">=</button>
+            <button className="btn" id="zero">0</button>
+          </div>
+        </div>
+        <p>Coded by Brunno Mota</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
