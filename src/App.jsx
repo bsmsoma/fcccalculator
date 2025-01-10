89ebb87c91fe13dbc3275d4bc7dd415eaf74f92e
bsmsoma <soma.bsm@gmail.com>
2025-01-10 18:27:41 -0300

    just another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,87 @@
 import './App.scss'
+import React from 'react'
 
 function App() {
-  
+  const [display, setDisplay] = React.useState(0);
+  const [currentNumber, setCurrentNumber] = React.useState(null);
 
+  const handleNumbers = (e) => {
+    const value = (e.target.innerText);
+    setDisplay(prev => prev == "0" ? value : prev += value);
+  }
+
+  function clearDisplay () {
+    setDisplay('0');
+    setCurrentNumber(0);
+  }
+  function handleOperators () {
+    setCurrentNumber(display);
+    setDisplay('0');
+  }
+  function handleEquals () {
+    const result = eval(`${currentNumber} ${display}`);
+    setDisplay(result);
+  }
+  function handleDecimal () {
+    if (!display.includes('.')) {
+      setDisplay(display + '.');
+    }
+  }
+  function  handleAdd () {
+    handleOperators();
+    setDisplay('+');
+  }
+  function handleSubtract () {
+    handleOperators();
+    setDisplay('-');
+  }
+  function handleMultiply () {
+    handleOperators();
+    setDisplay('*');
+  }
+  function handleDivide () {
+    handleOperators();
+    setDisplay('/');
+  }
+
+  
   return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     <>
       <div className="container ">
         <div className="calculator">
-          <div className="display" id="display">0</div>
+          <div className="display" id="display">{display}</div>
           <div className="buttons">
-            <button className="btn clear" id="clear">AC</button>
-            <button className="btn operator" id="divide">/</button>
-            <button className="btn operator" id="multiply">*</button>
-            <button className="btn operator" id="minus">-</button>
-            <button className="btn" id="serven">7</button>
-            <button className="btn" id="eight">8</button>
-            <button className="btn" id="nine">9</button>
-            <button className="btn operator" id="plus">+</button>
-            <button className="btn" id="five">4</button>
-            <button className="btn" id="five">5</button>
-            <button className="btn" id="six">6</button>
-            <button className="btn" id="decimal">.</button>
-            <button className="btn" id="one">1</button>
-            <button className="btn" id="two">2</button>
-            <button className="btn" id="three">3</button>
-            <button className="btn equals" id="equals">=</button>
-            <button className="btn" id="zero">0</button>
+            <button className="btn clear" id="clear" onClick={clearDisplay}>AC</button>
+            <button className="btn operator" id="divide" onClick={handleDivide}>/</button>
+            <button className="btn operator" id="multiply" onClick={handleMultiply}>*</button>
+            <button className="btn operator" id="subtract" onClick={handleSubtract}>-</button>
+            <button onClick={handleNumbers}className="btn" id="seven">7</button>
+            <button onClick={handleNumbers}className="btn" id="eight">8</button>
+            <button onClick={handleNumbers}className="btn" id="nine">9</button>
+            <button className="btn operator" id="add" onClick={handleAdd}>+</button>
+            <button onClick={handleNumbers}className="btn" id="four">4</button>
+            <button onClick={handleNumbers}className="btn" id="five">5</button>
+            <button onClick={handleNumbers}className="btn" id="six">6</button>
+            <button className="btn" id="decimal" onClick={handleDecimal}>.</button>
+            <button onClick={handleNumbers}className="btn" id="one">1</button>
+            <button onClick={handleNumbers}className="btn" id="two">2</button>
+            <button onClick={handleNumbers} className="btn" id="three">3</button>
+            <button className="btn equals" id="equals" onClick={handleEquals}>=</button>
+            <button onClick={handleNumbers} className="btn" id="zero">0</button>
           </div>
         </div>
         <p>Coded by Brunno Mota</p>
