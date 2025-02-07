9e85e7da53e3997562d267a96db2a20761b83f74
Brunno Mota <soma.bsm@gmail.com>
2025-02-07 20:40:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,62 +4,103 @@ import React from 'react'
 function App() {
   const [display, setDisplay] = React.useState(0);
   const [currentNumber, setCurrentNumber] = React.useState(null);
-
+  const [currentOperator, setCurrentOperator] = React.useState(null);
+  const [historyBool, setHistoryBool] = React.useState(false)
+  const operators = ['/', '+', '*'];
+  console.log(currentOperator)
   const handleNumbers = (e) => {
     const value = (e.target.innerText);
-    setDisplay(prev => prev == "0" ? value : prev += value);
+    if (historyBool) {
+      setDisplay("")
+      setHistoryBool(false)
+    }
+    setDisplay(prev => prev == "0" ? value : prev += value)
   }
 
-  function clearDisplay () {
+  function clearDisplay() {
     setDisplay('0');
-    setCurrentNumber(0);
+    setCurrentNumber(null);
+    setCurrentOperator(null)
+    setHistoryBool(false)
   }
-  function handleOperators () {
+
+  function handleOperators() {
+    if (operators.includes(display)) {
+      return
+    }
+    else if (display == '-') {
+      setDisplay("-")
+      return
+    }
     setCurrentNumber(display);
-    setDisplay('0');
+    setHistoryBool(true);
   }
-  function handleEquals () {
-    const result = eval(`${currentNumber} ${display}`);
+  function handleEquals() {
+    const result = eval(`${currentNumber} ${currentOperator} ${display}`)
+    setCurrentNumber(result);
     setDisplay(result);
+    setHistoryBool(true)
   }
-  function handleDecimal () {
+  function handleDecimal() {
     if (!display.includes('.')) {
       setDisplay(display + '.');
     }
   }
-  function  handleAdd () {
+  function handleAdd() {
+    if (display == "+") {
+      return
+    }
+    else if (currentNumber && display && !historyBool) {
+      setCurrentOperator("+")
+      return handleEquals();
+    }
     handleOperators();
+    setCurrentOperator("+")
     setDisplay('+');
   }
-  function handleSubtract () {
+  function handleSubtract() {
+    if (operators.includes(display)) {
+      setDisplay(prev => `-${display}`) //PAREI AQUI... PORQUE NAO SEI COMO CONCATENAR.
+      handleOperators()
+      console.log("rodou")
+      return
+    }
+    else if (currentNumber && display && !historyBool) {
+      setCurrentOperator("-")
+      return handleEquals();
+    }
     handleOperators();
+    setCurrentOperator("-")
     setDisplay('-');
   }
-  function handleMultiply () {
+  function handleMultiply() {
+    if (display == "*") {
+      return
+    }
+    else if (currentNumber && display && !historyBool) {
+      setCurrentOperator("*")
+      return handleEquals();
+    }
     handleOperators();
+    setCurrentOperator("*")
     setDisplay('*');
   }
-  function handleDivide () {
+  function handleDivide() {
+    if (display == "/") {
+      return
+    }
+    else if (currentNumber && display && !historyBool) {
+      setCurrentOperator("/")
+      return handleEquals();
+    }
     handleOperators();
+    setCurrentOperator("/")
     setDisplay('/');
   }
 
-  
+
   return (
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
     <>
       <div className="container ">
         <div className="calculator">
@@ -69,16 +110,16 @@ function App() {
             <button className="btn operator" id="divide" onClick={handleDivide}>/</button>
             <button className="btn operator" id="multiply" onClick={handleMultiply}>*</button>
             <button className="btn operator" id="subtract" onClick={handleSubtract}>-</button>
-            <button onClick={handleNumbers}className="btn" id="seven">7</button>
-            <button onClick={handleNumbers}className="btn" id="eight">8</button>
-            <button onClick={handleNumbers}className="btn" id="nine">9</button>
+            <button onClick={handleNumbers} className="btn" id="seven">7</button>
+            <button onClick={handleNumbers} className="btn" id="eight">8</button>
+            <button onClick={handleNumbers} className="btn" id="nine">9</button>
             <button className="btn operator" id="add" onClick={handleAdd}>+</button>
-            <button onClick={handleNumbers}className="btn" id="four">4</button>
-            <button onClick={handleNumbers}className="btn" id="five">5</button>
-            <button onClick={handleNumbers}className="btn" id="six">6</button>
+            <button onClick={handleNumbers} className="btn" id="four">4</button>
+            <button onClick={handleNumbers} className="btn" id="five">5</button>
+            <button onClick={handleNumbers} className="btn" id="six">6</button>
             <button className="btn" id="decimal" onClick={handleDecimal}>.</button>
-            <button onClick={handleNumbers}className="btn" id="one">1</button>
-            <button onClick={handleNumbers}className="btn" id="two">2</button>
+            <button onClick={handleNumbers} className="btn" id="one">1</button>
+            <button onClick={handleNumbers} className="btn" id="two">2</button>
             <button onClick={handleNumbers} className="btn" id="three">3</button>
             <button className="btn equals" id="equals" onClick={handleEquals}>=</button>
             <button onClick={handleNumbers} className="btn" id="zero">0</button>
